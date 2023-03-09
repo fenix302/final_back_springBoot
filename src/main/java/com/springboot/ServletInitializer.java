1e87e36df26f8f8c8dfe94d0964de1e463eed1c1
flyepic4 <flyepic4@gmail.com>
2023-03-09 23:07:36 +0900

    이름 수정

--- src/main/java/com/springboot/ServletInitializer.java
+++ src/main/java/com/springboot/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FinalProjectApplication.class);
+		return application.sources(final_back_springBootApplication.class);
 	}
 
 }
