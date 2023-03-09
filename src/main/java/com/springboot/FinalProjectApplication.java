3912b778f8c220bbbb0319cf2fa6331e333328f9
flyepic4 <flyepic4@gmail.com>
2023-03-09 22:38:37 +0900

    어플리케이션이름변경

--- src/main/java/com/springboot/FinalProjectApplication.java
+++ src/main/java/com/springboot/FinalProjectApplication.java
@@ -3,16 +3,13 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.springboot.board.controller.FreeBoardController;
 
 @EntityScan(basePackages = {"com.springboot"})
 @SpringBootApplication
-public class GuroCCApplication {
+public class FinalProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GuroCCApplication.class, args);
+		SpringApplication.run(FinalProjectApplication.class, args);
 		
 		System.out.println("====== 시작 =======");
 	}
